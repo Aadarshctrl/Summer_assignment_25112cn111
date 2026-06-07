37f3427f01132206593fc0affbab67f236e2cff3
Aadarsh Mishra <mishraaadarsh0807@gmail.com>
2026-06-07 23:11:46 +0530

    day7 done

---
+++ day_7/recursivereversaenumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int reversenumber(int n , int rev=0) {
+    if(n==0) {
+        return rev;
+    }
+    return reversenumber(n/10 , rev*10+n%10);
+}
+int main() {
+    int n;
+cout<<"enter number";
+cin>>n;
+cout<<reversenumber(n)<<" ";
+return 0;
+}
+
+
